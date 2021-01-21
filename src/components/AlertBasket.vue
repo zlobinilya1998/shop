3dd0c433d26ca1270f9912dda6bc13cc66178b4a
zlobin_ilya <qwertin1998@gmail.com>
2021-01-21 21:01:30 +0300

    upload

--- src/components/AlertBasket.vue
+++ src/components/AlertBasket.vue
@@ -1,51 +1,55 @@
 <template>
-  <v-card v-if="showBasket" class="basket pa-3" outlined>
-    <v-container class="d-flex flex-column pa-0">
-      <v-card-text class="d-flex justify-space-between pa-0 mt-2 ">
-        <span>Товаров на сумму:</span>
-        <span class="font-weight-black">{{ Math.ceil(getPrice) }}$</span>
-      </v-card-text>
-    </v-container>
-    <v-divider></v-divider>
-    <template v-for="(product, i) in getBasket">
-      <v-list-item class="pa-0" :key="i">
-        <v-list-item-content class="pa-0">
-          <v-container class="d-flex justify-space-between pa-0 mt-2">
-            <div :style="basketStyle" class="overline font-weight-medium">
-              {{ product.price }}$
-            </div>
-            <v-icon class="closeButton" @click="deleteProduct(i)"
-              >mdi-close-circle</v-icon
-            >
-          </v-container>
-          <v-container class="pa-0 d-flex justify-center">
-            <v-list-item-avatar
-              class="mb-5"
-              size="inherit"
-              max-height="150"
-              max-width="100"
-              tile
-              color="grey"
-              ><v-img :src="product.image" :alt="product.image"
-            /></v-list-item-avatar>
-          </v-container>
+  <transition name="basketTransition">
+    <v-card v-if="showBasket" class="basket pa-3" outlined>
+      <v-container class="d-flex flex-column pa-0">
+        <v-card-text class="d-flex justify-space-between pa-0 mt-2 ">
+          <span>Товаров на сумму:</span>
+          <span class="font-weight-black">{{ Math.ceil(getPrice) }}$</span>
+        </v-card-text>
+      </v-container>
+      <v-divider></v-divider>
+      <transition-group name="fade" mode="out-in">
+        <template v-for="(product, i) in getBasket">
+          <v-list-item class="pa-0" :key="`product#${i}`">
+            <v-list-item-content class="pa-0">
+              <v-container class="d-flex justify-space-between pa-0 mt-2">
+                <div :style="basketStyle" class="overline font-weight-medium">
+                  {{ product.price }}$
+                </div>
+                <v-icon class="closeButton" @click="deleteProduct(i)"
+                  >mdi-close-circle</v-icon
+                >
+              </v-container>
+              <v-container class="pa-0 d-flex justify-center">
+                <v-list-item-avatar
+                  class="mb-5"
+                  size="inherit"
+                  max-height="150"
+                  max-width="100"
+                  tile
+                  color="grey"
+                  ><v-img :src="product.image" :alt="product.image"
+                /></v-list-item-avatar>
+              </v-container>
 
-          <v-container class="d-flex flex-column pa-0 mb-5">
-            <v-list-item-subtitle
-              >Количество :
-              <span class="font-weight-medium">{{
-                product.quantity
-              }}</span></v-list-item-subtitle
-            >
-            <v-list-item-subtitle :style="{ color: 'black' }">{{
-              getTitle(product.title)
-            }}</v-list-item-subtitle>
-          </v-container>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider :key="i"></v-divider>
-    </template>
-  </v-card>
+              <v-container class="d-flex flex-column pa-0 mb-5">
+                <v-list-item-subtitle
+                  >Количество :
+                  <span class="font-weight-medium">{{
+                    product.quantity
+                  }}</span></v-list-item-subtitle
+                >
+                <v-list-item-subtitle :style="{ color: 'black' }">{{
+                  getTitle(product.title)
+                }}</v-list-item-subtitle>
+              </v-container>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider :key="i"></v-divider>
+        </template>
+      </transition-group>
+    </v-card>
+  </transition>
 </template>
 
 <script>
@@ -95,6 +99,20 @@ export default {
 </script>
 
 <style>
+.basketTransition-enter-active,
+.basketTransition-leave-active {
+  transition: all 0.4s;
+}
+.basketTransition-enter {
+  transform: translateY(-2rem);
+  opacity: 0;
+}
+
+.basketTransition-leave-to {
+  transform: translateX(10rem);
+  opacity: 0;
+}
+
 .closeButton:hover {
   color: #f66161 !important;
 }
@@ -105,5 +123,18 @@ export default {
   min-width: 15rem;
   width: 10%;
   overflow: auto;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.4s;
+}
+.fade-enter {
+  transform: translateY(-2rem);
+  opacity: 0;
+}
+
+.fade-leave-to {
+  transform: translateY(-1rem);
+  opacity: 0;
 }
 </style>
